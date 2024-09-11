b31846531f7565883dcfa08328bfa35c2a1c5d3f
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2024-09-11 14:28:13 -0500

    Crea el repositorio

---
+++ src/main/java/com/example/sistemaEducativo/SistemaEducativoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaEducativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEducativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEducativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemaEducativo/SistemaEducativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaEducativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEducativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
